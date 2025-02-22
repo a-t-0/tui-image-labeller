eafdbe8a6be2f898ffb4358c1700b7ef2a52fd77
a <you@example.com>
2025-02-22 23:43:34 +0100

    Wrote first test on CLI.

--- src/tui_labeller/__main__.py
+++ src/tui_labeller/__main__.py
@@ -1,6 +1,22 @@
 """Entry point for the project."""
 
-from src.tui_labeller.adder import add_two
+from argparse import ArgumentParser, Namespace
 
-some_val: int = 2
-print(f"Hello world. {some_val}+2={add_two(x=some_val)}")
+from src.tui_labeller.tuis.cli.questions.ask_receipt import (
+    build_receipt_from_cli,
+)
+from tui_labeller.arg_parser.arg_parser import create_arg_parser, verify_args
+
+parser: ArgumentParser = create_arg_parser()
+args: Namespace = verify_args(parser=parser)
+
+
+if __name__ == "__main__":
+    input("hi")
+    if args.tui == "CLI":
+
+        build_receipt_from_cli(
+            receipt_owner_account_holder="account_placeholder",
+            receipt_owner_bank="bank_placeholder",
+            receipt_owner_account_holder_type="account_type_placeholder",
+        )
---
+++ src/tui_labeller/arg_parser/arg_parser.py
@@ -0,0 +1,88 @@
+"""Parses the CLI args."""
+
+import argparse
+import os
+from argparse import ArgumentParser, Namespace
+
+from hledger_preprocessor.dir_reading_and_writing import assert_dir_exists
+from typeguard import typechecked
+
+
+@typechecked
+def create_arg_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description="Generate labels for images using CLI/TUI."
+    )
+
+    # Required args.
+    parser.add_argument(
+        "-i",
+        "--image-path",
+        type=str,
+        required=True,
+        help="Path to an image.",
+    )
+    parser.add_argument(
+        "-t",
+        "--tui",
+        type=str,
+        required=True,
+        help="Which TUI you would like to use for labelling.",
+    )
+    parser.add_argument(
+        "-o",
+        "--output-json-dir",
+        type=str,
+        required=True,
+        help="Where your output json will be stored.",
+    )
+
+    return parser
+
+
+@typechecked
+def assert_file_exists(*, filepath: str) -> None:
+    """Asserts that the given file exists.
+
+    Args:
+      filepath: The path to the file.
+
+    Raises:
+      FileNotFoundError: If the file does not exist.
+    """
+
+    if not os.path.isfile(filepath):
+        raise FileNotFoundError(f"File '{filepath}' does not exist.")
+
+
+@typechecked
+def assert_dir_exists(*, dirpath: str) -> None:
+    """Asserts that the given directory exists.
+
+    Args:
+      dirpath: The path to the directory.
+
+    Raises:
+      FileNotFoundError: If the directory does not exist.
+    """
+
+    if not os.path.isdir(dirpath):
+        raise FileNotFoundError(f"Directory '{dirpath}' does not exist.")
+
+
+@typechecked
+def verify_args(*, parser: ArgumentParser) -> Namespace:
+    args: Namespace = parser.parse_args()
+
+    # Verify output directory for jsons exist.
+    assert_dir_exists(dirpath=args.output_json_dir)
+
+    # Verify the input image exists.
+    assert_file_exists(filepath=args.image_path)
+
+    # Verify the chosen TUI method is supported.
+    if args.tui not in [
+        "CLI",
+    ]:
+        raise NotImplemented("That TUI is not supported.")
+    return args
