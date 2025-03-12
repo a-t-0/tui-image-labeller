6f2e202b7f4d2659bc28e649353c35c1e3b2aa36
a <you@example.com>
2025-03-12 19:10:25 +0100

    Ensured goes from top to bottom and back with up down.

--- src/tui_labeller/tuis/urwid/InputValidationQuestions.py
+++ src/tui_labeller/tuis/urwid/InputValidationQuestions.py
@@ -1,6 +1,5 @@
 import urwid
 
-from src.tui_labeller.file_read_write_helper import write_to_file
 from src.tui_labeller.tuis.urwid.autocomplete_filtering import (
     get_filtered_suggestions,
 )
@@ -57,70 +56,68 @@ class InputValidationQuestions:
             unhandled_input=self.handle_unhandled_input_major,
         )
 
-    def handle_unhandled_input_major(self, key):
-        # TODO: determine if you should move this into the "keypress" method
-        # of the InputValidationQuestion object.
-        if key == "enter":
-            current_pos = self.pile.focus_position
-            next_pos = current_pos + 1
-            # Check if the next position is within the valid range of input boxes
-            if next_pos < len(self.inputs):
-                self.pile.focus_position = next_pos
-                focused_widget = self.pile.focus
-                if isinstance(focused_widget, urwid.AttrMap):
-                    focused_widget.base_widget.update_autocomplete()
-            return  # Prevent raising ValueE
+    def move_to_next_answer(self, current_pos, key):
+        total_inputs = len(self.inputs)
+        # Calculate next position with wrap-around
+        if current_pos == total_inputs - 1:  # At last question
+            next_pos = 0  # Go to first
+        elif current_pos == 0 and key == "up":  # At first question going up
+            next_pos = total_inputs - 1  # Go to last
+        else:
+            next_pos = current_pos + (1 if key in ["enter", "down"] else -1)
 
-        # TODO: determine if you should move this into the "keypress" method
-        # of the InputValidationQuestion object.
-        if key == "tab":
-            write_to_file(
-                filename="eg.txt",
-                content=f"Focused_widget={focused_widget}",
-                append=False,
-            )
-            print(f"Afocused_widget={focused_widget}")
-            current_pos = self.pile.focus_position
+        # Only move if next_pos is within valid range
+        if 0 <= next_pos < total_inputs:
+            self.pile.focus_position = next_pos
             focused_widget = self.pile.focus
             if isinstance(focused_widget, urwid.AttrMap):
+                focused_widget.base_widget.update_autocomplete()
+            return True
+        return False
+
+    def handle_unhandled_input_major(self, key):
+
+        # Handle Enter key
+        if key == "enter":
+            current_pos = self.pile.focus_position
+            move_to_next_answer(current_pos)
+            return
+
+        # Handle Tab key with autocomplete
+        if key == "tab":
+            current_pos = self.pile.focus_position
+            focused_widget = self.pile.focus
+
+            if isinstance(focused_widget, urwid.AttrMap):
                 input_widget = focused_widget.base_widget
-                # Get the filtered suggestions based on current input text
                 remaining_suggestions = get_filtered_suggestions(
                     input_text=input_widget.edit_text,
                     available_suggestions=input_widget.suggestions,
                 )
-                # If there's exactly one suggestion, apply it
                 if len(remaining_suggestions) == 1:
                     new_text = remaining_suggestions[0]
                     input_widget.set_edit_text(new_text)
-                    input_widget.set_edit_pos(
-                        len(new_text)
-                    )  # Move cursor to the end
+                    input_widget.set_edit_pos(len(new_text))
+                    move_to_next_answer(current_pos)
             else:
                 print(f"focused_widget={focused_widget}")
-            return  # Prevent raising ValueError
+            return
 
+        # Handle Down key
         if key == "down":
             current_pos = self.pile.focus_position
-            if current_pos == len(self.inputs) - 1:  # If at last question
-                self.pile.focus_position = 0  # Go to first question
-                focused_widget = self.pile.focus
-                if isinstance(focused_widget, urwid.AttrMap):
-                    focused_widget.base_widget.update_autocomplete()
+            move_to_next_answer(current_pos)
+            return
 
+        # Handle Up key
         if key == "up":
             current_pos = self.pile.focus_position
-            if current_pos == 0:  # If at first question
-                self.pile.focus_position = (
-                    len(self.inputs) - 1
-                )  # Go to last question
-                focused_widget = self.pile.focus
-                if isinstance(focused_widget, urwid.AttrMap):
-                    focused_widget.base_widget.update_autocomplete()
-        # TODO: if cursor is at the first question and up is pressed, go to last question.
+            move_to_next_answer(current_pos)
+            return
+            # TODO: if cursor is at the first question and up is pressed, go to last question.
 
-        # TODO: if cursor is at the last question and down is pressed, go to first question.
-        # raise ValueError(f"STOPPED at:{key}")
+            # TODO: if cursor is at the last question and down is pressed, go to first question.
+            # raise ValueError(f"STOPPED at:{key}")
 
     def run(self):
         def update_autocomplete(widget, new_text):
